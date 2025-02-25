48e093a6c3f15a884b874910b64449e559b23bc9
Nischay <nischu1107@gmail.com>
2025-02-25 11:52:26 +0530

    Newer version

--- add.c
+++ add.c
@@ -1,1 +1,7 @@
-hihi , i am Nischay kumar. I am currently pursuing B.tch CSE in Amity university.
+hih
+i , i am Nischay kumar. I am currently pursuing B.tch CSE in Amity university.
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+
